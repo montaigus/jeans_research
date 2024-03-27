eea807a9d63e5187c1abe98040e048a058e3d5d8
Montaigus <montaigus@hotmail.com>
2024-03-27 11:43:21 +0100

    un bon nettoyage

--- src/App.jsx
+++ src/App.jsx
@@ -16,96 +16,18 @@ const App = () => {
     },
   });
 
-  const location = window.location;
-  let code;
-
-  const queryString = location.search;
-  console.log(queryString);
-  const params = new URLSearchParams(queryString);
-  code = params.get("code");
-
-  if (code) sendCodeToServer(code);
-
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="appContainer">{<MainElement code={code} />}</div>
+      <div className="appContainer">{<MainElement />}</div>
     </QueryClientProvider>
   );
 };
 
 const MainElement = (props) => {
-  const code = props.code;
-
-  return (
-    <>
-      {code ? (
-        <p>merci !</p>
-      ) : (
-        <a
-          href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=dkli55f6f0gijol1iaiyrhn1b9n3em&redirect_uri=http://localhost:5173&scope=chat:read"
-          className="link_connect"
-        >
-          Authoriser l'application
-        </a>
-      )}
-    </>
-  );
+  return <></>;
 };
 
 main();
-
-async function main() {
-  console.log("======================= start main =======================");
-  // const authProvider = new Anonym();
-  const bot = new ChatClient({
-    channels: ["Tonton"],
-    readOnly: true,
-  });
-  await bot.connect();
-  console.log("connecté au chat de Tonton");
-  bot.onMessage((channel, user, message, msg) => {
-    console.log(`msg id : ${msg.id} user : ${user}: ${message}`);
-  });
-
-  bot.onBan((channel, user, msg) => {
-    console.log(
-      `Cet utilisateur a été ban : ${user}, pour le message suivant : ${msg.isResponseTo}`
-    );
-  });
-
-  bot.onMessageRemove((channel, user, msg) => {
-    console.log(`Le message de ${user} a été modéré : ${msg.params.message}`);
-  });
-}
-
-async function sendCodeToServer(code) {
-  const serverUrl = "http://localhost:3000/authorized";
-
-  // Données à envoyer dans la requête POST
-  const data = { code };
-
-  // Options de la requête
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-
-  try {
-    const response = await fetch(serverUrl, options);
-
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP! Statut : ${response.status}`);
-    }
-
-    const responseData = await response.json();
-    console.log("Réponse du serveur :", responseData);
-  } catch (error) {
-    console.error("Erreur lors de la requête POST :", error.message);
-  }
-}
 
 const container = document.getElementById("page");
 const root = createRoot(container);
--- src/index.js
+++ src/index.js
@@ -21,47 +21,8 @@ const server = await app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-//?ça servait pour l'OAtuh client : authorisation du broadcaster. Pour l'instant, c'est inutile.
-// app.post("/authorized", (req, res) => {
-//   const authorizationCode = req.body.code;
-//   main(authorizationCode);
-//   res.send("c'est fait");
-// });
-
-//?ça c'est l'ancienne façon, on va essayer autre chose.
-// async function main(authorizationCode) {
-//   console.log("======================= start main =======================");
-//   const appToken = await getAppToken();
-//   getValidate(appToken);
-//   const broadcaster_id = await getIdFromPseudo("montaigus", appToken);
-//   console.log("broadcasterId  = " + broadcaster_id);
-//   const userToken = await getUserToken(authorizationCode);
-//   await getValidate(userToken);
-//   //await getModo(broadcaster_id, userToken);
-//   console.log("début du websocket");
-
-//   // Connexion au chat Twitch via WebSocket
-//   const chatWebSocketUrl = `wss://irc-ws.chat.twitch.tv`;
-//   const ws = new WebSocket(chatWebSocketUrl);
-
-//   ws.on("open", () => {
-//     console.log("Connected to Twitch Chat WebSocket");
-//     ws.send(`PASS oauth:${userToken}`);
-//     ws.send(`NICK blabla`); // Utilisez un nom d'utilisateur anonyme
-//     ws.send(`JOIN #${broadcaster_id}`); // Joindre le canal spécifique
-//   });
-
-//   ws.on("message", (data) => {
-//     console.log(`Received message from Twitch Chat WebSocket: ${data}`);
-//     // Traitement des messages du chat ici
-//   });
-
-//   console.log("======================== end main ========================");
-// }
-
 async function main() {
   console.log("======================= start main =======================");
-  // const authProvider = new Anonym();
   const bot = new ChatClient({
     channels: ["Tonton"],
     readOnly: true,
