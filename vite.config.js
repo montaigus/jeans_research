08b5b07f03e9e39cf9dca3d0785c6b85033947e6
Montaigus <montaigus@hotmail.com>
2024-04-09 11:20:25 +0200

    Test vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "./src",
-  base: "http://jeans-research.vercel.app",
+  base: "http://jeans-research.vercel.app/",
 });
