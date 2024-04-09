532ed9c2a60305e0920af45156def7c0604717bc
Montaigus <montaigus@hotmail.com>
2024-04-09 13:26:37 +0200

    ecore vercel

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "./src",
-  base: "./",
-  esbuild: {
-    jsxFactory: "React.createElement",
-    jsxFragment: "React.Fragment",
-    jsxInject: `import React from 'react'`,
-  },
 });
