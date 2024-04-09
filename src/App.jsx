256fed675c5b38f07b5d5e3f98f617dbfc659f94
Montaigus <montaigus@hotmail.com>
2024-04-09 13:33:30 +0200

    first commit en branch css_dev : lifting

--- src/App.jsx
+++ src/App.jsx
@@ -88,57 +88,63 @@ const App = () => {
 
   return (
     <>
-      <div className="header">Les recherches de Jean</div>
+      <div className="header">
+        <div className="title">Les recherches de Jean</div>
+      </div>
       <div className="appContainer">
-        <input
-          type="text"
-          className="input_channel"
-          placeholder="Chaine à suivre"
-          value={channel}
-          onChange={(e) => setChannel(e.target.value)}
-        />
-        {!connected && (
-          <button className="button_connect" onClick={handleConnect}>
-            Se connecter
-          </button>
-        )}
-        {connected && (
-          <button className="button_disconnect" onClick={handleDisconnect}>
-            Se déconnecter
-          </button>
-        )}
-        {connected && (
-          <a
-            className="button_download"
-            href={`${serverUrl}/download-json?channel=${channel}`}
-          >
-            Télécharger
-          </a>
-        )}
-        {connected && <button className="button_clear">Nettoyer</button>}
-        <div className="chat_holder">
-          {!queryResultChat.data && (
-            <div className="message">Rien à afficher</div>
+        <div className="search_bar">
+          <input
+            type="text"
+            className="input_channel"
+            placeholder="Chaine à suivre"
+            value={channel}
+            onChange={(e) => setChannel(e.target.value)}
+          />
+          {!connected && (
+            <button className="button_connect" onClick={handleConnect}>
+              Se connecter
+            </button>
           )}
-          {queryResultChat.data &&
-            queryResultChat.data.map((msg) => (
-              <div
-                className="message"
-                key={msg.data.id}
-              >{`${msg.data.user} : ${msg.data.message}`}</div>
-            ))}
+          {connected && (
+            <button className="button_disconnect" onClick={handleDisconnect}>
+              Se déconnecter
+            </button>
+          )}
+          {connected && (
+            <a
+              className="button_download"
+              href={`${serverUrl}/download-json?channel=${channel}`}
+            >
+              Télécharger
+            </a>
+          )}
+          {connected && <button className="button_clear">Nettoyer</button>}
         </div>
-        <div className="removed_holder">
-          {!queryResultRemoved.data && (
-            <div className="message">Rien à afficher</div>
-          )}
-          {queryResultRemoved.data &&
-            queryResultRemoved.data.map((msg) => (
-              <div
-                className="message"
-                key={msg.data.id}
-              >{`${msg.data.user} : ${msg.data.message}`}</div>
-            ))}
+        <div className="displayer">
+          <div className="chat_holder">
+            {!queryResultChat.data && (
+              <div className="message">Rien à afficher</div>
+            )}
+            {queryResultChat.data &&
+              queryResultChat.data.map((msg) => (
+                <div
+                  className="message"
+                  key={msg.data.id}
+                >{`${msg.data.user} : ${msg.data.message}`}</div>
+              ))}
+          </div>
+          <div className="removed_holder">
+            {!queryResultRemoved.data && (
+              <div className="message">Rien à afficher</div>
+            )}
+            {queryResultRemoved.data &&
+              queryResultRemoved.data.map((msg) => (
+                <div
+                  className="message"
+                  key={msg.data.id}
+                >{`${msg.data.user} : ${msg.data.message}`}</div>
+              ))}
+          </div>
         </div>
       </div>
     </>
