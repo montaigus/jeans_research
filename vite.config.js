416ca7d4f3618099b546c7730be0724f39ff732b
Montaigus <montaigus@hotmail.com>
2024-04-09 11:29:21 +0200

    remodif vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "./src",
-  base: "http://jeans-research.vercel.app/",
+  base: "./",
   esbuild: {
     jsxFactory: "React.createElement",
     jsxFragment: "React.Fragment",
